26642cb4b4b1fd289ca27e49e7235430ea60ee90
dawit <dawit.y.abera@nordstrom.com>
2020-05-05 23:09:41 -0700

    added landing page

---
+++ src/containers/Home.container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.container.css';
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="landing">
+        <h1>NOC-S3</h1>
+        <p>A simple file uploader to AWS S3. </p>
+      </div>
+    </div>
+  );
+}
